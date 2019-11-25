1b34738d2916a468c743e4875de2b26ad90afd89
Roniel Valdez <roniel-razer@pop-os.localdomain>
2019-11-25 13:26:01 -0400

    Initial server and  routes setup

---
+++ servers/routes.go
@@ -0,0 +1,30 @@
+package servers
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+)
+
+type Sample struct {
+	Slug  string `json:"slug"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Get("/{id}", SampleRoute)
+	return router
+}
+
+func SampleRoute(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	samples := Sample{
+		Slug:  id,
+		Title: "Hello World",
+		Body:  "A simple hello world",
+	}
+	render.JSON(w, r, samples)
+}
