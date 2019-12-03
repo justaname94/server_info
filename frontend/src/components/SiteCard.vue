a1dc459b7537ca7a64781eb50f1cda21dff62288
Roniel Valdez <roniel_valdez@outlook.com>
2019-12-03 14:37:37 -0400

    Set up boostrapvue and SiteCard component layout

---
+++ frontend/src/components/SiteCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="cards">
+    <b-card
+      header="xataka.com"
+      title="Xataka - Tecnología y gadgets, móviles, informática, electrónica"
+    >
+      <b-list-group>
+        <b-list-group-item>
+          <strong>Grade:</strong>
+          <b-badge>A</b-badge>
+        </b-list-group-item>
+        <b-list-group-item>
+          <strong>Previous grade:</strong>
+          <b-badge>B</b-badge>
+        </b-list-group-item>
+        <b-list-group-item>
+          <strong>Logo:</strong>
+          <img height="36px" width="36px" src="../assets/logo.png" alt />
+        </b-list-group-item>
+        <b-list-group-item>Is not down</b-list-group-item>
+        <b-list-group-item>Have not changed lately</b-list-group-item>
+      </b-list-group>
+      <hr />
+      <h4>Servers</h4>
+      <div>
+        <b-table striped :items="items"></b-table>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          address: "13.35.125.83",
+          grade: "A",
+          country: "US",
+          owner: "Amazon Technologies Inc. (AT-88-Z)"
+        },
+        {
+          address: "13.35.125.87",
+          grade: "A",
+          country: "US",
+          owner: "Amazon Technologies Inc. (AT-88-Z)"
+        },
+        {
+          address: "13.35.125.4",
+          grade: "A",
+          country: "US",
+          owner: "Amazon Technologies Inc. (AT-88-Z)"
+        },
+        {
+          address: "13.35.125.72",
+          grade: "A",
+          country: "US",
+          owner: "Amazon Technologies Inc. (AT-88-Z)"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import App from "./App.vue";
+
+Vue.use(BootstrapVue);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
