3e0226bb41f80ddfc54b8f98ab8223bda61c235a
Roniel Valdez <roniel_valdez@outlook.com>
2019-12-06 16:01:23 -0400

    Finish SiteCard component and continue search page

--- frontend/src/components/SiteCard.vue
+++ frontend/src/components/SiteCard.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="card-wrapper">
-    <b-card :header="website.domain" :title="website.title">
-      <b-list-group>
-        <b-list-group-item>
-          <strong>Grade:</strong>
-          <b-badge>{{ website.grade }}</b-badge>
-        </b-list-group-item>
-        <b-list-group-item v-if="website.previousGrade">
-          <strong>Previous grade:</strong>
-          <b-badge>{{ website.previousGrade }}</b-badge>
-        </b-list-group-item>
-        <b-list-group-item>
-          <strong>Logo:</strong>
-          <img height="36px" width="36px" src="../assets/logo.png" alt />
-        </b-list-group-item>
-        <b-list-group-item>
-          <span v-if="website.isDown">Is Down</span>
-          <span v-else>Is not down</span>
-        </b-list-group-item>
-        <b-list-group-item>Have not changed lately</b-list-group-item>
-      </b-list-group>
-      <hr />
-      <h4>Servers</h4>
-      <div>
-        <b-table striped :items="website.servers"></b-table>
-      </div>
-    </b-card>
-  </div>
+  <b-card :header="website.domain" :title="website.title" class="mt-2">
+    <b-list-group>
+      <b-list-group-item>
+        <strong>Grade:</strong>
+        <b-badge>{{ website.grade }}</b-badge>
+      </b-list-group-item>
+      <b-list-group-item v-if="website.previousGrade">
+        <strong>Previous grade:</strong>
+        <b-badge>{{ website.previousGrade }}</b-badge>
+      </b-list-group-item>
+      <b-list-group-item>
+        <strong>Logo:</strong>
+        <img height="36px" width="36px" v-if="!website.isDown" :src="logo" alt />
+      </b-list-group-item>
+      <b-list-group-item>
+        <span v-if="website.isDown">Is Down</span>
+        <span v-else>Is not down</span>
+      </b-list-group-item>
+      <b-list-group-item>Have not changed lately</b-list-group-item>
+    </b-list-group>
+    <hr />
+    <h4>Servers</h4>
+    <div>
+      <b-table striped :items="website.servers"></b-table>
+    </div>
+  </b-card>
 </template>
 
 <script>
@@ -36,12 +34,11 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    logo: function() {
+      return `${this.$http.options.root.slice(0, -1)}${this.website.logo}`;
+    }
   }
 };
 </script>
-
-<style scoped>
-.card-wrapper {
-  margin-top: 10px;
-}
-</style>
--- frontend/src/pages/Search.vue
+++ frontend/src/pages/Search.vue
@@ -2,13 +2,17 @@
   <div>
     <h2 class="text-center">Search Website's information</h2>
     <b-form @submit.prevent="getSiteInfo">
-      <b-form-input v-model="url" placeholder="Ex: google.com"></b-form-input>
+      <b-form-input v-model="url" :disabled="loading" placeholder="Ex: google.com"></b-form-input>
       <br />
       <div class="text-center">
         <b-button size="lg" type="submit" variant="primary">Search</b-button>
       </div>
     </b-form>
     <app-site-card v-if="Object.keys(website).length > 0" :website="website"></app-site-card>
+    <div v-if="loading" class="text-center mt-3">
+      <b-spinner label="Loading..."></b-spinner>
+      <p>Please wait while we get the data, this could take a while...</p>
+    </div>
   </div>
 </template>
 
@@ -21,19 +25,30 @@ export default {
   },
   data() {
     return {
+      loading: false,
       url: "",
       website: {}
     };
   },
   methods: {
     getSiteInfo(evt) {
+      this.website = {};
+      this.loading = true;
       this.$http
         .get(`sites/${this.url}`)
         .then(response => {
           return response.json();
         })
         .then(data => {
-          this.website = data;
+          if (data.message !== undefined && data.message === "IN_PROGRESS") {
+            // Recursively retry the request until is ready
+            setTimeout(() => {
+              this.getSiteInfo(evt);
+            }, 3000);
+          } else {
+            this.loading = false;
+            this.website = data;
+          }
         });
     }
   }
